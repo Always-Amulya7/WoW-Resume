d68b0ccab94cb0de843ac2b15252e8bb3851aca9
Always-Amulya7 <amulyashrivastava7@gmail.com>
2026-04-08 01:35:09 +0530

    Model Upgrade

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,5 @@ import { genkit } from "genkit";
 import { googleAI } from "@genkit-ai/googleai";
 export const ai = genkit({
   plugins: [googleAI()],
-  model: "googleai/gemini-2.0-flash",
+  model: "googleai/gemini-2.5-flash",
 });
